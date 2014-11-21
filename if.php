381f2ba660d23ee204012a1d173520ac50efc3d4
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-21 11:17:46 -0600

    Day 4 revision

--- if.php
+++ if.php
@@ -9,17 +9,15 @@ if ($a < $b) {
     echo "$a is less than $b\n";
 }
 
-if ($b > $a) {
+else {
     // output the appropriate result
-    echo "$b is greater $a\n";
+    echo "$b is less than $a\n";
 }
-
 if ($b >= $c) {
     // output the appropriate result
     echo "$b is greater than or equal to $c\n";
 }
-
-if ($b <= $c) {
+else {
     // output the appropriate result
     echo "$b is less than or equal to $c\n";
 }
@@ -29,19 +27,18 @@ if ($b == $c) {
     echo "$b is equal $c\n";
 }
 
-if ($b === $c) {
+elseif ($b === $c) {
     // output the appropriate result
-    echo "$b is equal $c\n";
+    echo "$b is identical $c\n";
 }
 
 // output the appropriate result
-if ($b <> $c) {
+elseif ($b != $c) {
+    echo "$b is not identical to $c\n";
+}
+else {
     echo "$b is not equal to $c\n";
 }
 
 // output the appropriate result
-if ($b !== $c) {
-    echo "$b is not identical to $c\n";
-}
 
-?>
