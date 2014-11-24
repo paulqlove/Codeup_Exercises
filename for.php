7ea80e90b5cd68b09dc8055b73370e982d2f85c8
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-24 16:52:52 -0600

    increments

--- for.php
+++ for.php
@@ -5,10 +5,14 @@ fwrite(STDOUT, 'Please enter your numbers' . PHP_EOL) ;
 //user inputs numbers
 $start = trim(fgets(STDIN));
 $end = trim(fgets(STDIN));
+echo "========" . PHP_EOL;
+//user chooses increment
+fwrite(STDOUT, 'Choose the increment' . PHP_EOL);
+$count = trim(fgets(STDIN));
 //line break	
 echo "========" . PHP_EOL;
 //for loop
-	for ($i= $start; $i <= $end ; $i++) { 
+	for ($i= $start; $i <= $end ; $i += $count) { 
 //echo out the count 	
 	echo $i . PHP_EOL;
 		}
