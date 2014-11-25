0a1cdc5c042564f12c405ac7d4f40139da13e266
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-24 18:11:43 -0600

    number 3

--- for.php
+++ for.php
@@ -1,19 +1,35 @@
 <?php
 
 //ask user to input numbers
-fwrite(STDOUT, 'Please enter your numbers' . PHP_EOL) ;
-//user inputs numbers
+
+// Get starting number
+fwrite(STDOUT, 'Please enter your starting number' . PHP_EOL) ;
 $start = trim(fgets(STDIN));
+
+// Get Ending number
+fwrite(STDOUT, 'Please enter your ending number' . PHP_EOL) ;
 $end = trim(fgets(STDIN));
+
 echo "========" . PHP_EOL;
+
 //user chooses increment
 fwrite(STDOUT, 'Choose the increment' . PHP_EOL);
 $count = trim(fgets(STDIN));
+
+
+
+$count = ($count == '') ? 1 : $count;
+
+
 //line break	
 echo "========" . PHP_EOL;
 //for loop
-	for ($i= $start; $i <= $end ; $i += $count) { 
-//echo out the count 	
+	
+		
+			
+for ($i= $start; $i <= $end; $i += $count) { 
+	//echo out the count 	
 	echo $i . PHP_EOL;
-		}
 
+}			
+	
