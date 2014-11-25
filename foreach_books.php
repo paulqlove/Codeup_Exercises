c2f018053101a9408918033faa67ffea0bc623f9
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-25 15:15:54 -0600

    books

--- foreach_books.php
+++ foreach_books.php
@@ -1,24 +1,35 @@
 <?php
 
-$books = array(
-    'The Hobbit' => array(
+$books = [
+    'The Hobbit' => [
         'published' => 1937,
         'author' => 'J. R. R. Tolkien',
         'pages' => 310
-    ),
-    'Game of Thrones' => array(
+    ],
+    'Game of Thrones' => [
         'published' => 1996,
         'author' => 'George R. R. Martin',
         'pages' => 835
-    ),
-    'The Catcher in the Rye' => array(
+    ],
+    'The Catcher in the Rye' => [
         'published' => 1951,
         'author' => 'J. D. Salinger',
         'pages' => 220
-    ),
-    'A Tale of Two Cities' => array(
+    ],
+    'A Tale of Two Cities' => [
         'published' => 1859,
         'author' => 'Charles Dickens',
         'pages' => 544
-    )
-);
+    ]
+];
+
+foreach($books as $title => $info) {
+    
+    echo 'Book: ' . $title . PHP_EOL;
+    echo 'Author: ' . $author . "{$info['author']}" . PHP_EOL;
+    echo 'Year: ' . $published . "{$info['published']}" . PHP_EOL;
+    echo 'Pages: ' . $pages . "{$info['pages']}" . PHP_EOL;
+    echo PHP_EOL;
+    
+}
+  
