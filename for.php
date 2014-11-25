d3133a58f38ebd9baf4a390d77b60e100d763c9b
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-24 23:29:11 -0600

    number 4

--- for.php
+++ for.php
@@ -1,35 +1,40 @@
 <?php
 
-//ask user to input numbers
+
 
 // Get starting number
 fwrite(STDOUT, 'Please enter your starting number' . PHP_EOL) ;
+
 $start = trim(fgets(STDIN));
 
 // Get Ending number
 fwrite(STDOUT, 'Please enter your ending number' . PHP_EOL) ;
+
 $end = trim(fgets(STDIN));
 
 echo "========" . PHP_EOL;
 
 //user chooses increment
 fwrite(STDOUT, 'Choose the increment' . PHP_EOL);
+
 $count = trim(fgets(STDIN));
 
-
-
+//if $count is identical to ' ' then it is true and equal to 1. if false then it is equal to $count;
 $count = ($count == '') ? 1 : $count;
-
 
 //line break	
 echo "========" . PHP_EOL;
-//for loop
-	
-		
-			
-for ($i= $start; $i <= $end; $i += $count) { 
-	//echo out the count 	
-	echo $i . PHP_EOL;
 
-}			
+
+//for loop	
+	if(is_numeric($start . $end . $count)) {	
+		
+		for ($i= $start; $i <= $end; $i += $count){ 
+		//echo out the count 	
+		echo $i . PHP_EOL;
+		}
+	} else {
+		echo 'Error: Please enter numbers only' . PHP_EOL;
+		}
+			
 	
