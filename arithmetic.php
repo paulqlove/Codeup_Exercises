9eb658601a26367fc17c3d8ca552487e476c1361
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-26 14:47:28 -0600

    argument

--- arithmetic.php
+++ arithmetic.php
@@ -1,31 +1,36 @@
 <?php
-$a = 5;
+$a = 'five';
 $b = 3;
 
-function add($a, $b) {
+if(is_numeric($a) && is_numeric($b)) {
+		
+		function add($a, $b) {
 
-    echo $a + $b . PHP_EOL;
-}
-	add($a, $b);
+		    echo $a + $b . PHP_EOL;
+		}
+			add($a, $b);
 
-function subtract($a, $b) {
-    echo $a - $b .PHP_EOL;
-}
-	subtract($a, $b);
+		function subtract($a, $b) {
+		    echo $a - $b .PHP_EOL;
+		}
+			subtract($a, $b);
 
-function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
+		function multiply($a, $b) {
+		    echo $a * $b . PHP_EOL;
 
-}
-	multiply($a, $b);
+		}
+			multiply($a, $b);
 
-function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
-}
-	divide($a, $b);
+		function divide($a, $b) {
+		    echo $a / $b . PHP_EOL;
+		}
+			divide($a, $b);
 
-function modulus($a, $b){
-	echo $a % $b . PHP_EOL;
-}
-	modulus($a, $b);
-?>
+		function modulus($a, $b){
+			echo $a % $b . PHP_EOL;
+		}
+			modulus($a, $b);
+		} else {
+				echo "ERROR: Numbers only please!" . PHP_EOL;
+			}
+
