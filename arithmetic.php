6558623699b5c6e52a44a111e7574430315966b7
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-26 13:47:59 -0600

    arithmetic.php

--- arithmetic.php
+++ arithmetic.php
@@ -6,17 +6,25 @@ function add($a, $b) {
     echo $a + $b . PHP_EOL;
 }
 	add(2, 5);
+
 function subtract($a, $b) {
     echo $a - $b .PHP_EOL;
 }
 	subtract(7,5);
+
 function multiply($a, $b) {
     echo $a * $b . PHP_EOL;
 
 }
 	multiply(7,2);
+
 function divide($a, $b) {
     echo $a / $b . PHP_EOL;
 }
 	divide(50, 5);
+
+function modulus($a, $b){
+	echo $a % $b . PHP_EOL;
+}
+	modulus(62, 3);
 ?>
