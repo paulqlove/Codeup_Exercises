1103987c3dfb152d00cb22c879062fadb1ef7e32
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-26 11:00:34 -0600

    two class exercises

---
+++ break_continue.php
@@ -0,0 +1,21 @@
+<?php
+
+for($i = 1; $i <= 100; $i++) {
+		
+		echo $i . PHP_EOL;
+		if ($i == 10) {
+			
+			break;
+		}
+		
+	} echo "done" . PHP_EOL;
+	/*if($i % 2 == 0) {
+		echo $i . PHP_EOL;
+		echo "This is an odd number" . PHP_EOL;
+		
+
+		}	*/
+
+		
+
+
---
+++ switch.php
@@ -0,0 +1,46 @@
+<?php
+
+ // Set the default timezone
+ date_default_timezone_set('America/Chicago');
+
+ // Get Day of Week as number
+ // 1 (for Monday) through 7 (for Sunday)
+ $dayOfWeek = date('N');
+
+
+ switch($dayOfWeek) {
+     case 1:
+     	echo 'Monday' . PHP_EOL;
+     	break;
+         // Output Monday
+     case 2:
+     	echo 'Tuesday' . PHP_EOL;
+     	break;
+     case 3:
+     	echo 'Wednesday' . PHP_EOL;
+     	break;
+     case 4:
+		echo 'Thursday' . PHP_EOL;
+		break;
+	case 5:
+		echo 'Friday' . PHP_EOL;
+		break;
+	case 6:
+		echo 'Saturday' . PHP_EOL;
+		break;
+	case 7:
+		echo 'Sunday' . PHP_EOL;
+		break;
+         // Output Tuesday
+     // etc through day 7
+ }
+ if($dayOfWeek == 1 ){
+ 	echo 'Monday' . PHP_EOL;
+ }elseif($dayOfWeek == 2){
+ 	echo 'Tuesday' . PHP_EOL;
+ }elseif($dayOfWeek == 3) {
+ 	echo 'Wednesday' . PHP_EOL;
+
+}
+
+
