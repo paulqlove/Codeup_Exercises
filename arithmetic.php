2c6379b8378ce64ab1c10f5359ac89f794179789
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-11-27 06:55:18 -0600

    number4

--- arithmetic.php
+++ arithmetic.php
@@ -1,11 +1,11 @@
 <?php
 
 $a = 16;
-$b = 'ten';
+$b = 'five';
 
  function errorMsg($param1, $param2){
 	echo "Error: Numbers Both Arguments must be numbers" . PHP_EOL;
-	echo "\$a = $param1, and \$b = $param2 " . PHP_EOL;
+	echo "\$a = $param1,  and  \$b = $param2"  . PHP_EOL;
 }
 		
 		function add($a, $b) {
@@ -36,19 +36,21 @@ $b = 'ten';
 			}
 		}	
 		function divide($a, $b) {
-			if(($a == 0) || ($b == 0)) {
+			if(($a === 0) || ($b === 0)) {
 				echo "Error: can not be divdided" . PHP_EOL;
-			}
-			if(is_numeric($a) && is_numeric($b)){
-				echo $a / $b . PHP_EOL;
-			} else {
-				echo "ERROR: Numbers ONly" . PHP_EOL;
-			}
-		}
-			
-
+				echo errorMsg($a, $b) . PHP_EOL;
+			} elseif (is_numeric($a) && is_numeric($b)) {
+					echo $a / $b . PHP_EOL;
+				} else {
+						echo "ERROR: Numbers ONly" . PHP_EOL;
+						}	
+		}		
+		
 		function modulus($a, $b) {
-			if(is_numeric($a) && is_numeric($b)) {
+			if(($a === 0) || ($b === 0)) {
+				echo "Error: can not use zero" . PHP_EOL;
+				
+			} elseif(is_numeric($a) && is_numeric($b)) {
 			
 				echo $a % $b . PHP_EOL;
 
