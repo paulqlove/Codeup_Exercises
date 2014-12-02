1568773dddbe49dd6d66fcba717b88f78e33c34d
Paul Love <paullove@Pauls-MacBook-Pro.local>
2014-12-02 14:55:37 -0600

    number 2

--- search_arrays.php
+++ search_arrays.php
@@ -3,3 +3,24 @@
  $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
  $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+
+ 
+
+function searchNames($searchFor, $arrayToSearch){
+ 	$result = array_search($searchFor, $arrayToSearch);
+ 	var_dump($result);
+ 
+	 if($result === false){
+	 	echo $searchFor . " is not in the array" . PHP_EOL;
+	 		return 'false';
+	 	}else {
+	 		
+	 		echo  $searchFor . " is in the array" . PHP_EOL;
+	 		return 'True';
+	 	}
+	} 
+ 
+ $searchFor = 'Mike';
+ $arrayToSearch = $names; 
+echo searchNames($searchFor,$arrayToSearch) . PHP_EOL;
